24bdc41ebf4182c84ab2bac3bb691a0686b1d154
Fatima Albachir <55485414+dewdropbashabebars@users.noreply.github.com>
2025-05-20 19:52:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// alert('wellcome to iecastle');
+const menu = document.getElementById('icon-mobile');
+const list = document.getElementById('list-mobile');
+menu.onclick = function() {
+        list.classList.toggle("mobile-nav");
+
+    }
+    //////////////////////////////////
